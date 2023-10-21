8df4b8bb88b26711215e464d1dbf752346a87062
khayrul200822 <khayrulislam200822@gmail.com>
2023-10-21 12:54:30 -0700

    latest news section updated

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ const product_title = document.querySelectorAll(".p_title");
 const single_slider_box = document.querySelector(".single_slider");
 
 
- //text moving with mouse 
+//text moving with mouse 
 // single_slider_box.addEventListener("mouseover", () => {
 //   document.addEventListener("mousemove", parallax);
 // })
@@ -20,7 +20,7 @@ const single_slider_box = document.querySelector(".single_slider");
 
 // function parallax (e) {
 //   product_title.forEach(function(move) {
-   
+
 //     let x = e.clientX *.2
 //     let y = e.clientY *.2
 //     if(x < 100 && y < 100){
@@ -28,9 +28,9 @@ const single_slider_box = document.querySelector(".single_slider");
 //     }else{
 //       move.style.transform = `translate(0px, 0px)`;
 //     }
- 
-  
-    
+
+
+
 //   })
 // }
 
@@ -45,7 +45,7 @@ function toggleSearchBar(displayValue, opacityValue) {
   desktop_search_bar.style.display = displayValue;
   desktop_search_bar.style.opacity = opacityValue;
 }
-desktop_search_bar.addEventListener('scroll', function(event) {
+desktop_search_bar.addEventListener('scroll', function (event) {
   event.preventDefault();
 });
 desktop_search_icon.addEventListener("click", () => {
@@ -68,7 +68,7 @@ product_item.addEventListener("mouseover", () => {
   if (product_item_visible) {
     product_item_message.style.transform = "translate(0px, 30%)";
     product_item_message.style.opacity = "0";
-   
+
   } else {
     product_item_message.style.transition = "transform 0.3s, opacity 0.3s"; // Add a transition property
     product_item_message.style.transform = "translate(0px, 0px)";
@@ -103,12 +103,12 @@ search_bar_icon.addEventListener("click", () => {
     mobile_search_bar.style.display = "unset";
     setTimeout(() => {
       mobile_search_bar.style.visibility = "visible";
-       mobile_search_bar.style.transform = "translate(0px, 0px)";
-    mobile_search_bar.style.opacity = "1";
+      mobile_search_bar.style.transform = "translate(0px, 0px)";
+      mobile_search_bar.style.opacity = "1";
     }, 0);
-   
+
   }
-  
+
   isMobileSearchVisible = !isMobileSearchVisible;
 });
 
@@ -116,13 +116,13 @@ search_bar_icon.addEventListener("click", () => {
 
 
 // mobile animated menubar icon
-document.addEventListener('DOMContentLoaded', function() {
-    var thirdButton = document.querySelector('.third-button');
-    var animatedIcon = document.querySelector('.nav_animated_icon');
+document.addEventListener('DOMContentLoaded', function () {
+  var thirdButton = document.querySelector('.third-button');
+  var animatedIcon = document.querySelector('.nav_animated_icon');
 
-    thirdButton.addEventListener('click', function() {
-        animatedIcon.classList.toggle('open');
-    });
+  thirdButton.addEventListener('click', function () {
+    animatedIcon.classList.toggle('open');
+  });
 });
 
 // product section button script here
@@ -147,40 +147,33 @@ for (let i = 0; i < 5; i++) {
 }
 
 //****************owl carousel script**********
- //owl carousel script
- $(document).ready(function () {
+//owl carousel script
+$(document).ready(function () {
 
   $(".owl-carousel").owlCarousel({
 
-      slideSpeed: 300,
-      paginationSpeed: 400,
-      items: 1,
-      dots: false,
-      nav:true,
-      loop: true, 
-       itemsDesktop: false,
-      itemsDesktopSmall: false,
-      itemsTablet: false,
-      itemsMobile: false,// Enable loop mode
+    slideSpeed: 300,
+    paginationSpeed: 400,
+    items: 1,
+    dots: false,
+    nav: true,
+    loop: true,
+    itemsDesktop: false,
+    itemsDesktopSmall: false,
+    itemsTablet: false,
+    itemsMobile: false,// Enable loop mode
     autoplay: true, // Enable auto play
     autoplayTimeout: 3000, // Set the time interval (1 second in this case)
     autoplayHoverPause: true,
     animateOut: "fadeOut",
     animateIn: "fadeIn",
-  
+
   });
 
-});
 
-
-
+})
 
 //Mixitup plugin call
 var mixer = mixitup('.product_slider');
-
-
-
-
-
 
 
